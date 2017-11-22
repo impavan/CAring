b0e3c24d211721296f2462458279decd4791bfc9
subhashish.s <subhashish.s@spurtreetech.in>
2017-11-22 17:10:10 +0530

    chnages according to keval's mail

--- src/pages/my-account/my-account.ts
+++ src/pages/my-account/my-account.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Events  } from 'ionic-angular';
 import { AlertProvider } from '../../providers/alert/alert';
+
+
 
 
 @IonicPage()
@@ -10,15 +12,37 @@ import { AlertProvider } from '../../providers/alert/alert';
 })
 export class MyAccountPage {
 
+  _auth:any;
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
+              public events: Events,
               public alertProvider:AlertProvider) {
+          }
+
+
+          ionViewWillEnter(){
+            this._auth = localStorage.getItem('auth_token');
           }
 
 
   comingSoon() {
     this.alertProvider.presentToast('coming soon..')
     return;
+  }
+
+  gotoLogin() {
+    
+    this.navCtrl.setRoot("LoginPage");
+    
+  }
+  logout() {
+    
+    localStorage.removeItem('auth_token');
+    localStorage.removeItem('phoneNum');
+    this.events.publish('user:login', false);
+    this.alertProvider.presentToast("You have been logged out..!")
+    this.navCtrl.setRoot("LoginPage");
+
   }
   
 
