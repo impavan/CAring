abf8eeb42c1162847283dbefbfc6ce75974c02e0
siddik49 <muhammed.s@spurtreetech.in>
2017-11-24 12:20:12 +0530

    added css changes

--- src/pages/my-account/my-account.ts
+++ src/pages/my-account/my-account.ts
@@ -44,7 +44,7 @@ export class MyAccountPage {
       
     localStorage.removeItem('auth_token');
     localStorage.removeItem('phoneNum');
-    // this.pushProvider.logoutWebengage();
+    this.pushProvider.logoutWebengage();
     this.events.publish('user:login', false);
     this.alertProvider.presentToast("You have been logged out..!")
     })
