4c337e9ba8f64afd3191bbd38e86193829b12194
siddik49 <muhammed.s@spurtreetech.in>
2017-12-07 13:04:58 +0530

    added permission for accessing location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { Geolocation } from '@ionic-native/geolocation';
 import { LaunchNavigator } from '@ionic-native/launch-navigator';
 import { ScreenOrientation } from '@ionic-native/screen-orientation';
+import { Diagnostic } from '@ionic-native/diagnostic';
 import { Camera } from '@ionic-native/camera';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AppVersion } from '@ionic-native/app-version';
@@ -56,6 +57,7 @@ import { PushProvider } from '../providers/push/push';
     StoreLocatorProvider,
     LoaderProvider,
     AlertProvider,
+    Diagnostic,
     LoaderProvider, AlertProvider,Geolocation,LaunchNavigator,
     Camera, AppVersion, ScreenOrientation, Device, Network, ExceptionHandlerProvider, AuthProvider, UserdataProvider, LoaderProvider, AlertProvider, RewardsProvider,
     NetworkProvider, DeviceProvider, ProfileProvider, InAppBrowser,
--- src/pages/store-locator/store-locator.ts
+++ src/pages/store-locator/store-locator.ts
@@ -2,10 +2,12 @@ import { Component, ViewChild,ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { LaunchNavigator } from '@ionic-native/launch-navigator';
+import { Diagnostic } from '@ionic-native/diagnostic';
 
 //All providers goes here
 import { StoreLocatorProvider } from '../../providers/store-locator/store-locator';
 import { LoaderProvider } from '../../providers/loader/loader';
+import { AlertProvider } from '../../providers/alert/alert';
 
 
 declare var google;
@@ -42,6 +44,8 @@ export class StoreLocatorPage {
               public storeLocatorProvider: StoreLocatorProvider, 
               private loaderProvider:LoaderProvider,
               private geolocation: Geolocation,
+              private diagnostic: Diagnostic,
+              private alertProvider:AlertProvider,
               private launchNavigator: LaunchNavigator, private elRef: ElementRef) {
     
             this.instoreData = navParams.get('instore') || '';
@@ -60,74 +64,85 @@ export class StoreLocatorPage {
 
 
   ngAfterViewInit() {
-    this.loadMap();
 
+    this.diagnostic.isLocationAuthorized().then(res => {
+      
+      console.log(res);
+      console.log("outside++++++++");
+
+      if (!res) {
+        this.diagnostic.requestLocationAuthorization('always').then(resp => {
+          console.log(resp)
+             console.log("inside++++++++");
+          if (resp == 'GRANTED')
+          this.loadMap();
+        })
+      }else
+      this.loadMap();
+    })  
+    
   }
 
-  // getStores() {
-  //   this.storeLocatorProvider.getStores().subscribe(res => {
-  //     this.locationList = res.data;
-  //     this.addMarkers(this.map, this.locationList);
-  //     this.loadFavList(this.locationList);
-  //   });
-  // }
+
 
   loadMap() {
 
-    this.geolocation.getCurrentPosition().then((resp) => {
+      this.geolocation.getCurrentPosition().then((resp) => {
      
-      console.log("Inside loadmap");
-       // resp.coords.latitude
-      // resp.coords.longitude
-      // let lat = "12.9716";
-      // let lng = "77.5946";
+        console.log("Inside loadmap");
+        // resp.coords.latitude
+        // resp.coords.longitude
+        // let lat = "12.9716";
+        // let lng = "77.5946";
 
-      this.getAllStores(resp.coords.latitude, resp.coords.latitude, 50);
-    // this.getAllStores(lat,lng, 50);
+        this.getAllStores(resp.coords.latitude, resp.coords.latitude, 50);
+        // this.getAllStores(lat,lng, 50);
      
-    this._myCurrentLocation = {
-      lat:resp.coords.latitude,
-      lng:resp.coords.longitude,
-    }
-    //   this._myCurrentLocation = {
-    //   lat:lat,
-    //   lng:lng,
-    // }
+        this._myCurrentLocation = {
+          lat: resp.coords.latitude,
+          lng: resp.coords.longitude,
+        }
+        //   this._myCurrentLocation = {
+        //   lat:lat,
+        //   lng:lng,
+        // }
      
-    let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
-      //  let latLng = new google.maps.LatLng(lat, lng);
+        let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+        //  let latLng = new google.maps.LatLng(lat, lng);
      
-    let mapOptions = {
-      center: latLng,
-      zoom: 12,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+        let mapOptions = {
+          center: latLng,
+          zoom: 12,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
      
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
  
-   }).catch((error) => {
+      }).catch((error) => {
     
-     console.log('Error getting location', error);
-      let lat = "3.1390";
-      let lng = "101.6869";
-      this.getAllStores(lat, lng, 50);
+      //   console.log('Error getting location', error);
+        let lat = "3.1390";
+        let lng = "101.6869";
+      //   this.getAllStores(lat, lng, 50);
      
-      this._myCurrentLocation = {
-      lat:lat,
-      lng:lng,
-      }
-      let latLng = new google.maps.LatLng(lat, lng); 
+      //   this._myCurrentLocation = {
+      //     lat: lat,
+      //     lng: lng,
+      //   }
+        let latLng = new google.maps.LatLng(lat, lng);
 
       
-    let mapOptions = {
-      center: latLng,
-      zoom: 12,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+        let mapOptions = {
+          center: latLng,
+          zoom: 12,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
      
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions); 
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      this.alertProvider.presentToast("Error in accessing your current location. Please provide permission to access location");
      
-    });
+      });
+     
   }
 
   loadFavList(locationList) {
