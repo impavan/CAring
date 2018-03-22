1991227706da499b9a2de98ceaa18ddb52453db3
Subhashsih Shrivas <subhashish.s@spurtreetech.in>
2018-03-22 17:53:26 +0530

    Edit profile updated

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -238,42 +238,42 @@ export class AuthProvider {
 
   //return User SMS Subscription //
 
-  getUserSMSSubscribtion() {
-    if (this.userObj.customer[0].custom_fields.field != null && this.userObj.customer[0].custom_fields.field.length > 0) {
-      let sms = this.userObj.customer[0].custom_fields.field.find(data => data.name == 'sms')
-      if (sms && sms.value != null)
-        return sms.value;
-      else
-        return '';
-    } else
-      return '';
-  }
+  // getUserSMSSubscribtion() {
+  //   if (this.userObj.customer[0].custom_fields.field != null && this.userObj.customer[0].custom_fields.field.length > 0) {
+  //     let sms = this.userObj.customer[0].custom_fields.field.find(data => data.name == 'sms')
+  //     if (sms && sms.value != null)
+  //       return sms.value;
+  //     else
+  //       return '';
+  //   } else
+  //     return '';
+  // }
 
    //return User SMS Subscription //
 
-   getUserEmailSubscribtion() {
-    if (this.userObj.customer[0].custom_fields.field != null && this.userObj.customer[0].custom_fields.field.length > 0) {
-      let email = this.userObj.customer[0].custom_fields.field.find(data => data.name == 'email')
-      if (email && email.value != null)
-        return email.value;
-      else
-        return '';
-    } else
-      return '';
-  }
+  //  getUserEmailSubscribtion() {
+  //   if (this.userObj.customer[0].custom_fields.field != null && this.userObj.customer[0].custom_fields.field.length > 0) {
+  //     let email = this.userObj.customer[0].custom_fields.field.find(data => data.name == 'email')
+  //     if (email && email.value != null)
+  //       return email.value;
+  //     else
+  //       return '';
+  //   } else
+  //     return '';
+  // }
 
   //return User (Return Customer) Detail //
 
-  getUserReturn() {
-    if (this.userObj.customer[0].custom_fields.field != null && this.userObj.customer[0].custom_fields.field.length > 0) {
-      let return_customer = this.userObj.customer[0].custom_fields.field.find(data => data.name == 'return_customer')
-      if (return_customer && return_customer.value != null)
-        return return_customer.value;
-      else
-        return '';
-    } else
-      return '';
-  }
+  // getUserReturn() {
+  //   if (this.userObj.customer[0].custom_fields.field != null && this.userObj.customer[0].custom_fields.field.length > 0) {
+  //     let return_customer = this.userObj.customer[0].custom_fields.field.find(data => data.name == 'return_customer')
+  //     if (return_customer && return_customer.value != null)
+  //       return return_customer.value;
+  //     else
+  //       return '';
+  //   } else
+  //     return '';
+  // }
 
 
   //returns User Occupation
