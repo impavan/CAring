ac454e6609f3be32c2ea796796cbc8366fa1441b
Kathiravan S <kathiravan.s@spurtreetech.com>
2018-08-21 12:13:15 +0530

    Done the UI changes in happenings page

--- src/pages/happenings/happenings.module.ts
+++ src/pages/happenings/happenings.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SharedModule } from '../../app/shared.module';
 import { HappeningsPage } from './happenings';
+import { InStorePage } from '../in-store/in-store'
 
 @NgModule({
   declarations: [
     HappeningsPage,
+    InStorePage
   ],
   imports: [
     IonicPageModule.forChild(HappeningsPage),
-    SharedModule
+    SharedModule,
+    
   ],
 })
 export class HappeningsPageModule {}
